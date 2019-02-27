4b93c3a2b0c8dc911592bc57db39cf2c0063b59a
Ernesto Jiménez <ernesto@bedu.org>
2019-02-26 20:51:49 -0600

    added movie detail

--- onomatopoeia_reviews/reviews/urls.py
+++ onomatopoeia_reviews/reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from reviews import views
 
 urlpatterns = [
-    path('movie-list/', views.movie_list)
+    path('movie-list/', views.movie_list),
+    path('movie-detail/<movie_id>/', views.movie_detail, name='movie-detail'),
 ]
--- onomatopoeia_reviews/reviews/views.py
+++ onomatopoeia_reviews/reviews/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from reviews.models import Movie
+from django.http import Http404
 
 
 def movie_list(request):
-    print(request.method)
     context = {
         'movies': Movie.objects.all()
     }
     return render(request, 'movie-list.html', context)
 
+def movie_detail(request, movie_id):
+    context = {
+        'movie': get_object_or_404(Movie, pk=movie_id)
+    }
+    # try:
+    #     movie = Movie.objects.get(pk=movie_id)
+    # except Movie.DoesNotExist:
+    #     return Http404()
+    return render(request, 'movie-detail.html', context)
 
 # for review in Review.objects.all():
 #     print(review.content)
