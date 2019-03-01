6cf04150a0ba2e1f18f8631b848e52c5c8bf8dc1
Ernesto Jiménez <ernesto@bedu.org>
2019-02-28 19:56:08 -0600

    Added template login

--- onomatopoeia_reviews/onomatopoeia_reviews/settings.py
+++ onomatopoeia_reviews/onomatopoeia_reviews/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+INTERNAL_IPS = ['127.0.0.1']
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'reviews',
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'onomatopoeia_reviews.urls'
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
--- onomatopoeia_reviews/onomatopoeia_reviews/urls.py
+++ onomatopoeia_reviews/onomatopoeia_reviews/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
@@ -5,5 +6,11 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('reviews.urls')),
-    path('login/', LoginView.as_view()),
+    path('login/', LoginView.as_view(template_name='login.html')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
