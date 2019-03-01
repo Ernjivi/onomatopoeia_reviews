5a97aa2794f8f098fbad6a2eead7a8a52f35a67b
Ernesto Jiménez <ernesto@bedu.org>
2019-02-28 19:20:35 -0600

    Clase 08

--- onomatopoeia_reviews/onomatopoeia_reviews/urls.py
+++ onomatopoeia_reviews/onomatopoeia_reviews/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('reviews.urls')),
+    path('login/', LoginView.as_view()),
 ]
--- onomatopoeia_reviews/reviews/views.py
+++ onomatopoeia_reviews/reviews/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 from reviews.models import Movie
-from django.http import Http404
+from reviews.forms import ReviewForm
 
 
 def movie_list(request):
@@ -9,15 +13,28 @@ def movie_list(request):
     }
     return render(request, 'movie-list.html', context)
 
+@login_required
 def movie_detail(request, movie_id):
-    context = {
-        'movie': get_object_or_404(Movie, pk=movie_id)
-    }
-    # try:
-    #     movie = Movie.objects.get(pk=movie_id)
-    # except Movie.DoesNotExist:
-    #     return Http404()
-    return render(request, 'movie-detail.html', context)
-
-# for review in Review.objects.all():
-#     print(review.content)
+    if request.method == 'GET':
+        context = {
+           'movie': get_object_or_404(Movie, pk=movie_id),
+           'movie_form': ReviewForm()
+        }
+        # try:
+        #     movie = Movie.objects.get(pk=movie_id)
+        # except Movie.DoesNotExist:
+        #     return Http404()
+        return render(request, 'movie-detail.html', context)
+    elif request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.movie_id = movie_id
+            review.save()
+            return HttpResponseRedirect(reverse('movie-detail', args=[movie_id]))
+        else:
+            context = {
+                'movie': get_object_or_404(Movie, pk=movie_id),
+                'movie_form': review_form
+            } 
+            return render(request, 'movie-detail.html', context)
